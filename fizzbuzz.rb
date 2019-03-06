efc156b39ab0b5b0ff02d11bbb34e8e065e03088
Ashley Miller <asm2fe@virginia.edu>
2019-03-06 01:45:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,14 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if ((number % 3 == 0) && (number % 5 == 0))
-    return "FizzBuzz"
+    "FizzBuzz"
+  end
   else if (number % 3 == 0)
-    return "Fizz"
+    "Fizz"
+  end
   else if (number % 5 ==0)
-    return "Buzz"
+    "Buzz"
+  end
   else
     return nil
   end
